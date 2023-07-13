9d346985ff99084b2cfdae08636d648a412f3475
eliasurieel <eliaasgarcia7@gmail.com>
2023-07-13 20:11:54 -0300

    [feature] - create folder structure, create header component and styles, add categories json file, and implement in app

---
+++ src/components/header/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet, } from "react-native";
+import { COLORS } from "../../themes";
+
+export const styles = StyleSheet.create({
+    container:{
+        marginTop:40,
+        backgroundColor: COLORS.primary,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+        	width: 0,
+        	height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+
+        elevation: 6,
+    },
+    title:{
+        fontSize: 20,
+        color: COLORS.text,
+        fontWeight: 'bold'
+
+    }
+});
---
+++ src/themes/colors/index.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+    primary: '#A97576',
+    secondary: '#8A5657',
+    text: '#000000',
+    background: '#F2F0FA',
+    black: '#000000',
+    white: '#FFFFFF',
+
+};
