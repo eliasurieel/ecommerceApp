062783b90a3ad8929231b2332cfd87f18de7b3f9
eliasurieel <eliaasgarcia7@gmail.com>
2023-07-27 23:45:47 -0300

    [feature] - add style and icons in bottom tab navigator

--- src/navigation/tabs.jsx
+++ src/navigation/tabs.jsx
@@ -1,7 +1,9 @@
+import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ShopNavigator from "./shop";
 import OrdersNavigator from "./orders";
 import CartNavigator from "./cart";
+import { COLORS } from "../themes";
 
 const BottomTab = createBottomTabNavigator()
 
@@ -9,11 +11,45 @@ const TabsNavigator = () => {
     return(
         <BottomTab.Navigator 
         initialRouteName="ShopTab" 
-        screenOptions={{headerShown: false,
+        screenOptions={{
+            headerShown: false,
+            tabBarLabelStyle:{
+                fontSize: 10,
+            },
+            tabBarStyle:{
+                backgroundColor: COLORS.white,
+            },
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarInactiveTintColor: COLORS.gray,
+            tabBarIconStyle:{
+                fontSize: 20,
+            }
+
         }}>
-            <BottomTab.Screen name="ShopTab" component={ShopNavigator} />
-            <BottomTab.Screen name="CartTab" component={CartNavigator} />
-            <BottomTab.Screen name="OrdersTab" component={OrdersNavigator} />
+            <BottomTab.Screen name="ShopTab" component={ShopNavigator} options={{
+                tabBarLabel: 'Shop',
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons name={ focused ? 'home' : 'home-outline'} size={size} color={color} />
+                ),
+            }} />
+            <BottomTab.Screen name="CartTab" component={CartNavigator} options={{
+                tabBarLabel: 'Cart',
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons name={ focused ? 'cart' : 'cart-outline'} size={size} color={color} />
+                ),
+                tabBarBadge: 2,
+                tabBarBadgeStyle:{
+                    backgroundColor: COLORS.secondary, 
+                    color: COLORS.white,
+                    fontSize: 11,
+                },
+            }} />
+            <BottomTab.Screen name="OrdersTab" component={OrdersNavigator} options={{
+                tabBarLabel: 'Orders',
+                tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons name={ focused ? 'file-tray' : 'file-tray-outline'} size={size} color={color} />
+                ),
+            }} />
         </BottomTab.Navigator>
     )
 }
